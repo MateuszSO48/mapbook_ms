06bbc5bbd396ff3a671d42e1ac825fe93803f2e8
MateuszSO48 <u56148@student.wat.edu.pl>
2025-10-29 09:10:21 +0100

    wip: added for loop with users

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+zmienna_1:str= "Mateusz"
+zmienna_2:str= "Michasia"
+zmienna_3:str= "Kasia"
+zmienna_4:str= "Asia"
+
+
+uzytkownicy:list=[zmienna_1, zmienna_2, zmienna_3, zmienna_4]
+for uzytkownik in uzytkownicy:
+    print(f"hello {uzytkownik}")
