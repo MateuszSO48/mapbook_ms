6b015eb8e259fff81fbcf00ca6ad46da31bf40f4
MateuszSO48 <u56148@student.wat.edu.pl>
2025-11-19 13:56:25 +0100

    feat: added markers

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class User:
         self.posts = posts
         self.img_url = img_url
         self.coords = self.get_coordinates()
+        self.marker = map_widget.set_marker(self.coords[0], self.coords[1], text=self.name)
 
     def get_coordinates(self):
         import requests
@@ -56,16 +57,23 @@ def user_info(users_data:list):
         listbox_lista_obiektow.insert(idx, f"{user.name} {user.location} {user.posts} posty")
 
 
+
+
+
 def delete_user(users_data:list):
     i = listbox_lista_obiektow.index(ACTIVE)
+    users_data[i].marker.delete()
     users_data.pop(i)
     user_info(users_data)
+
 
 def user_details(users_data:list):
     i = listbox_lista_obiektow.index(ACTIVE)
     label_imie_szczegoly_obiektu_wartosc.config(text=users_data[i].name)
     label_lokalizacja_szczegoly_obiektu.config(text=users_data[i].location)
     label_posty_szczegoly_obiektu_wartosc.config(text=users_data[i].posts)
+    map_widget.set_position(users_data[i].coords[0], users_data[i].coords[1])
+    map_widget.set_zoom(14)
 
 def edit_user(users_data:list):
     i = listbox_lista_obiektow.index(ACTIVE)
@@ -82,9 +90,14 @@ def update_user(users_data:list, i):
     users_data[i].location = entry_lokalizacja.get()
     users_data[i].posts = entry_posty.get()
     users_data[i].img_url = entry_img_url.get()
+
+    users_data[i].coords = users_data[i].get_coordinates()
+    users_data[i].marker.set_position(users_data[i].coords[0], users_data[i].coords[1])
+    users_data[i].marker.set_text(text=users_data[i].name)
+
     user_info(users_data)
 
-    button_dodaj_obiekt.config(text="Dodaj obiekt", command=lambda:add_user(users))
+    button_dodaj_obiekt.config(text="Dodaj obiekt", command=lambda:add_user(users_data))
     entry_name.delete(0, END)
     entry_lokalizacja.delete(0, END)
     entry_posty.delete(0, END)
@@ -185,6 +198,7 @@ label_posty_szczegoly_obiektu_wartosc.grid(row=1,column=5)
 
 #RAMKA MAPY
 map_widget = tkintermapview.TkinterMapView(ramka_mapa, width=1025, height=600, corner_radius=0)
+
 map_widget.set_position(52.0, 21.0)
 map_widget.set_zoom(6)
 map_widget.grid(row=0,column=0)
